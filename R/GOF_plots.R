33793519f8eab624106ad4c8e0292a22f7d28385
konkam <guillaume.konkamking.work@gmail.com>
2019-05-16 11:40:50 +0200

    put all plots together

--- R/GOF_plots.R
+++ R/GOF_plots.R
@@ -58,10 +58,11 @@ plotCDF_noncensored = function(fit){
   else{
     cdf = get_CDF_full_BNPdensity(fit = fit, xs = grid)
   }
-  ggplot2::ggplot(data = data.frame(data = grid, CDF = cdf), ggplot2::aes(x = data, y = CDF)) +
+  ggplot2::ggplot(data = data.frame(data = grid, CDF = cdf), aes(x = data, y = CDF)) +
     geom_line(colour= 'red') +
     theme_classic() +
-    stat_ecdf(data = data.frame(data), aes(y = NULL), geom = "step")
+    stat_ecdf(data = data.frame(data), aes(y = NULL), geom = "step") +
+    xlab("Data")
 }
 
 #' Plot the density and a histogram for non censored data.
@@ -86,9 +87,61 @@ plotPDF_noncensored = function(fit){
   else{
     pdf = get_PDF_full_BNPdensity(fit = fit, xs = grid)
   }
-  ggplot2::ggplot(data = data.frame(data = grid, PDF = pdf), ggplot2::aes(x = data, y = PDF)) +
+  ggplot2::ggplot(data = data.frame(data = grid, PDF = pdf), aes(x = data, y = PDF)) +
     geom_histogram(data = data.frame(data), aes(y = ..density..)) +
     geom_line(colour= 'red') +
-    theme_classic()
+    theme_classic() +
+    xlab("Data")
 
+}
+
+#' Plot the density and a histogram for non censored data.
+#'
+#' @param data The non censored dataset.
+#' @param fit The result of the fit, obtained through the function MixNRMI1 or MixNRMI2.
+#' @return Plot of the ensity and a histogram for non censored data.
+#' @examples
+#' set.seed(150520)
+#' data(acidity)
+#' out <- MixNRMI1(acidity, extras = TRUE, Nit = 100)
+#' plotPDF_noncensored(out)
+pp_plot_noncensored = function(fit){
+
+  data = fit$data
+
+  # grid = grid_from_data(data)
+
+  if(is_semiparametric(fit)){
+    cdf = get_CDF_semi_BNPdensity(fit = fit, xs = data)
+  }
+  else{
+    cdf = get_CDF_full_BNPdensity(fit = fit, xs = data)
+  }
+  ggplot2::ggplot(data = data.frame(x = cdf, y = ecdf(data)(data)), aes(x = x, y = y)) +
+    geom_point() +
+    geom_abline(slope = 1, intercept = 0, colour= 'red') +
+    theme_classic() +
+    xlab("Theoretical cumulative distribution") +
+    ylab("Empirical cumulative distribution")
+
+}
+
+#' Plot Goodness of fits graphical checks for non censored data
+#'
+#' @param fit
+#'
+#' @return A density plot with histogram, a cumulative density plot with the empirical cumulative distribution, and a percentile-percentile plot.
+#' @export
+#'
+#' @examples
+#' set.seed(150520)
+#' data(acidity)
+#' out <- MixNRMI1(acidity, extras = TRUE, Nit = 100)
+#' plotGOF_noncensored(out)
+plotGOF_noncensored = function(fit){
+
+  CDFplot = plotCDF_noncensored(fit)
+  PDFplot = plotPDF_noncensored(fit)
+  pplot = pp_plot_noncensored(fit)
+  gridExtra::grid.arrange(PDFplot, CDFplot, pplot)
 }
