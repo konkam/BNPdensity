ea837ee36385e2ba2cc7f12884b9bdfe5034da78
konkam <guillaume.konkamking.work@gmail.com>
2019-05-16 10:08:28 +0200

    updated tests

--- tests/testthat/test_main_functions_for_errors.R
+++ tests/testthat/test_main_functions_for_errors.R
@@ -1,6 +1,3 @@
-context("Test that the main functions run well")
-
-
 test_that("MixNRMI1 runs ok", {
   expect_output(str(MixNRMI1(rnorm(20), Nit = 50, extras = TRUE)), "List")
 })
@@ -8,8 +5,8 @@ test_that("MixNRMI2 runs ok", {
   expect_output(str(MixNRMI2(rnorm(20), Nit = 50, extras = TRUE)), "List")
 })
 test_that("MixNRMI1cens runs ok", {
-  expect_output(str(MixNRMI1cens(salinity$left, salinity$left, Nit = 50, extras = TRUE)), "List")
+  expect_output(str(MixNRMI1cens(salinity$left, salinity$right, Nit = 50, extras = TRUE)), "List")
 })
 test_that("MixNRMI2cens runs ok", {
-  expect_output(str(MixNRMI2cens(salinity$left, salinity$left, Nit = 50, extras = TRUE)), "List")
+  expect_output(str(MixNRMI2cens(salinity$left, salinity$right, Nit = 60, extras = TRUE)), "List")
 })
