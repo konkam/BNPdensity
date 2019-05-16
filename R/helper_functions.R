cbc40e6aad827c58ee15bc4d3cd80456e6628897
konkam <guillaume.konkamking.work@gmail.com>
2019-05-16 10:07:55 +0200

    updated docs

--- R/helper_functions.R
+++ R/helper_functions.R
@@ -14,14 +14,8 @@ is_semiparametric = function(fit){
 
 #' Repeat the common scale parameter of a semiparametric model to match the dimension of the location parameters.
 #'
-#' @param fit The result of the fit, obtained through the function MixNRMI1 or MixNRMI2.
-#' @return TRUE if the fit is a semiparametric model
-#' @examples
-#' set.seed(150520)
-#' data(acidity)
-#' x <- enzyme
-#' out <- MixNRMI1(enzyme, extras = TRUE)
-#' is_semiparametric(out)
+#' @param semiparametric_fit The result of the fit, obtained through the function MixNRMI1.
+#' @return an adequate list of vectors of sigmas
 fill_sigmas = function(semiparametric_fit){
   mapply(FUN = function(means, sigma){
     rep(sigma, length(means))
