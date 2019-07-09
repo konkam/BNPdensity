86b062758738bb4c5fb09d66fef1c3ff2dd76000
konkam <guillaume.konkamking.work@gmail.com>
2019-07-09 19:13:07 +0200

    expected number of clusters

--- R/expected_number_of_clusters_stable_process.R
+++ R/expected_number_of_clusters_stable_process.R
@@ -39,20 +39,20 @@ Cnk <- function(n, k, Gama) {
 Pkn_PY <- function(k, n, Alpha, Gama) {
   # exp(log_Vnk_PY(n = n, k = k, Alpha = Alpha, Gama = Gama) - k*log(Gama) + log_Cnk(n = n, k = k, Gama = Gama))
   # Using this form, the inaccuracies in Cnk (i.e. getting a negative number) do not give NaN.
-  # This error might be cancelled when computing the expected number of clusters.
+  # This error might be cancelled when computing the expected number of components.
   exp(log_Vnk_PY(n = n, k = k, Alpha = Alpha, Gama = Gama) - k * log(Gama)) * Cnk(n = n, k = k, Gama = Gama)
 }
 
 # Pkn_PY(3, 5, 0.2, 0.4)
 
 Pkn_Dirichlet <- function(k, n, Alpha) {
-  exp(k*log(Alpha) + log(abs(gmp::Stirling1(n, k))) - sum(log(Alpha+0:(n-1))))
+  exp(k * log(Alpha) + log(abs(gmp::Stirling1(n, k))) - sum(log(Alpha + 0:(n - 1))))
 }
 
 # Pkn_Dirichlet(3, 5, 0.2)
 
 
-expected_number_of_cluster_PY <- function(n, Alpha, Gama, ntrunc = NULL) {
+expected_number_of_components_PY <- function(n, Alpha, Gama, ntrunc = NULL) {
   if (is.null(ntrunc)) {
     ntrunc <- n
   } else if (ntrunc > n) ntrunc <- n
@@ -67,19 +67,20 @@ expected_number_of_cluster_PY <- function(n, Alpha, Gama, ntrunc = NULL) {
 
 
 
-#' Computes the expected number of clusters for a Dirichlet process.
+#' Computes the expected number of components for a Dirichlet process.
+#'
+#' @export
 #'
 #' @param n Number of data points
 #' @param Alpha Numeric constant. Total mass of the centering measure.
 #' @param ntrunc Level of truncation when computing the expectation. Defaults to n. If greater than n, it is fixed to n.
 #'
-#' @return A real value which approximates the expected number of clusters
-#' @export
+#' @return A real value which approximates the expected number of components
 #'
 #' Reference: P. De Blasi, S. Favaro, A. Lijoi, R. H. Mena, I. Prünster, and M. Ruggiero, “Are gibbs-type priors the most natural generalization of the dirichlet process?,” IEEE Trans. Pattern Anal. Mach. Intell., vol. 37, no. 2, pp. 212–229, 2015.
 #' @examples
-#' expected_number_of_cluster_Dirichlet(100, 1.2)
-expected_number_of_cluster_Dirichlet <- function(n, Alpha, ntrunc = NULL) {
+#' expected_number_of_components_Dirichlet(100, 1.2)
+expected_number_of_components_Dirichlet <- function(n, Alpha, ntrunc = NULL) {
   if (is.null(ntrunc)) {
     ntrunc <- n
   } else if (ntrunc > n) ntrunc <- n
@@ -92,48 +93,48 @@ expected_number_of_cluster_Dirichlet <- function(n, Alpha, ntrunc = NULL) {
   return(res)
 }
 
-# expected_number_of_cluster_PY(10, 0., 0.4)
+# expected_number_of_components_PY(10, 0., 0.4)
 
-#' Computes the expected number of clusters for a stable process.
+#' Computes the expected number of components for a stable process.
+#' @export
 #'
 #' @param n Number of data points
 #' @param Gama Numeric constant. 0 <= Gama <=1.
 #' @param ntrunc Level of truncation when computing the expectation. Defaults to n. If greater than n, it is fixed to n.
 #'
-#' @return A real value of type mpfr1 which approximates the expected number of clusters
-#' @export
+#' @return A real value of type mpfr1 which approximates the expected number of components
 #'
 #' In spite of the high precision arithmetics packages used for in function, it can be numerically unstable for small values of Gama.
 #' This is because evaluating a sum with alternated signs, in the generalised factorial coefficients, is tricky.
 #' Reference: P. De Blasi, S. Favaro, A. Lijoi, R. H. Mena, I. Prünster, and M. Ruggiero, “Are gibbs-type priors the most natural generalization of the dirichlet process?,” IEEE Trans. Pattern Anal. Mach. Intell., vol. 37, no. 2, pp. 212–229, 2015.
 #'
 #' @examples
-#' expected_number_of_cluster_stable(100, 0.8)
-#' Vectorize(expected_number_of_cluster_stable, vectorize.args = "ntrunc")(100, 0.8, c(5, 10, 20, 40))
-expected_number_of_cluster_stable <- function(n, Gama, ntrunc = NULL) {
+#' expected_number_of_components_stable(100, 0.8)
+#' Vectorize(expected_number_of_components_stable, vectorize.args = "ntrunc")(100, 0.8, c(5, 10, 20, 40))
+expected_number_of_components_stable <- function(n, Gama, ntrunc = NULL) {
   if (!requireNamespace("Rmpfr", quietly = TRUE) && !requireNamespace("gmp", quietly = TRUE)) {
     stop("Packages Rmpfr and gmp are needed for this function to work. Please install them.",
       call. = FALSE
     )
   }
-  expected_number_of_cluster_PY(n, 0, Gama, ntrunc = ntrunc)
+  expected_number_of_components_PY(n, 0, Gama, ntrunc = ntrunc)
 }
 
-#' Plot the prior number of clusters for a stable process and for a Dirichlet process with Alpha = 1.
+#' Plot the prior number of components for a stable process and for a Dirichlet process with Alpha = 1.
 #'
-#' This plots the prior distribution on the number of clusters for the stable process. The Dirichlet process is provided for comparison.
+#' This plots the prior distribution on the number of components for the stable process. The Dirichlet process is provided for comparison.
 #'
 #' @param n Number of data points
 #' @param Gama Numeric constant. 0 <= Gama <=1.
 #' @param Alpha Numeric constant. Total mass of the centering measure for the Dirichlet process.
 #' @param grid Level of truncation when computing the expectation. Defaults to n. If greater than n, it is fixed to n.
 #'
-#' @return A plot with the prior distribution on the number of clusters.
+#' @return A plot with the prior distribution on the number of components.
 #' @export
 #'
 #' @examples
-#' plot_prior_number_of_clusters(50, 0.4)
-plot_prior_number_of_clusters <- function(n, Gama, Alpha = 1, grid = NULL) {
+#' plot_prior_number_of_components(50, 0.4)
+plot_prior_number_of_components <- function(n, Gama, Alpha = 1, grid = NULL) {
   if (!requireNamespace("Rmpfr", quietly = TRUE) && !requireNamespace("gmp", quietly = TRUE)) {
     stop("Packages Rmpfr and gmp are needed for this function to work. Please install them.",
       call. = FALSE
@@ -141,12 +142,13 @@ plot_prior_number_of_clusters <- function(n, Gama, Alpha = 1, grid = NULL) {
   }
   if (is.null(grid)) grid <- 1:n
   grid <- unique(round(grid)) # Make sure it is a grid of integers
-  data.frame(K = grid, Pk = Rmpfr::toNum(Vectorize(Pkn_PY, vectorize.args = "k")(grid, n, 0, Gama)), Process = 'Stable') %>%
-    rbind(data.frame(K = grid, Pk = Vectorize(Pkn_Dirichlet, vectorize.args = "k")(grid, n, Alpha), Process = 'Dirichlet')) %>%
-    ggplot(aes_string(x = "K", y = "Pk", colour = 'factor(Process)', group = 'Process')) +
+  to_plot <- rbind(
+    data.frame(K = grid, Pk = Rmpfr::toNum(Vectorize(Pkn_PY, vectorize.args = "k")(grid, n, 0, Gama)), Process = "Stable"),
+    data.frame(K = grid, Pk = Vectorize(Pkn_Dirichlet, vectorize.args = "k")(grid, n, Alpha), Process = "Dirichlet")
+  )
+  ggplot(data = to_plot, aes_string(x = "K", y = "Pk", colour = "factor(Process)", group = "Process")) +
     geom_point() +
     geom_line() +
     theme_classic() +
-    viridis::scale_colour_viridis(discrete = T, name = 'Process')
+    viridis::scale_colour_viridis(discrete = T, name = "Process")
 }
-
