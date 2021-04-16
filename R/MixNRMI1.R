d01011c8df694608e384cc6419f6db5c49b671ba
konkam <guillaume.konkamking.work@gmail.com>
2021-04-16 19:10:56 +0200

    function using the log transfo + adaptive mcmc

--- R/MixNRMI1.R
+++ R/MixNRMI1.R
@@ -321,6 +321,148 @@ MixNRMI1 <-
     return(structure(res, class = "NRMI1"))
   }
 
+MixNRMI1bis <-
+  function(x, probs = c(0.025, 0.5, 0.975), Alpha = 1, Kappa = 0,
+           Gama = 0.4, distr.k = "normal", distr.p0 = 1, asigma = 0.5, bsigma = 0.5,
+           delta_S = 3, delta_U = 2, Meps = 0.01, Nx = 150, Nit = 1500,
+           Pbi = 0.1, epsilon = NULL, printtime = TRUE, extras = TRUE, adaptive = FALSE) {
+    if (is.null(distr.k)) {
+      stop("Argument distr.k is NULL. Should be provided. See help for details.")
+    }
+    if (is.null(distr.p0)) {
+      stop("Argument distr.p0 is NULL. Should be provided. See help for details.")
+    }
+    distr.k <- process_dist_name(distr.k)
+    distr.p0 <- process_dist_name(distr.p0)
+    tInit <- proc.time()
+    n <- length(x)
+    y <- x
+    xsort <- sort(x)
+    y[seq(n / 2)] <- mean(xsort[seq(n / 2)])
+    y[-seq(n / 2)] <- mean(xsort[-seq(n / 2)])
+    u <- 1
+    sigma <- 1
+    if (is.null(epsilon)) {
+      epsilon <- sd(x) / 4
+    }
+    xx <- seq(min(x) - epsilon, max(x) + epsilon, length = Nx)
+    Fxx <- matrix(NA, nrow = Nx, ncol = Nit)
+    fx <- matrix(NA, nrow = n, ncol = Nit)
+    R <- seq(Nit)
+    S <- seq(Nit)
+    U <- seq(Nit)
+    Nmt <- seq(Nit)
+    Allocs <- vector(mode = "list", length = Nit)
+    if (!adaptive) {
+      optimal_delta <- rep(delta_U, n)
+    }
+    else {
+      optimal_delta <- rep(NA, n)
+    }
+    if (extras) {
+      means <- vector(mode = "list", length = Nit)
+      weights <- vector(mode = "list", length = Nit)
+      Js <- vector(mode = "list", length = Nit)
+      delta_Us <- seq(Nit)
+    }
+    mu.p0 <- mean(x)
+    sigma.p0 <- sd(x)
+    for (j in seq(Nit)) {
+      if (floor(j / 500) == ceiling(j / 500)) {
+        cat("MCMC iteration", j, "of", Nit, "\n")
+      }
+      tt <- comp1(y)
+      ystar <- tt$ystar
+      nstar <- tt$nstar
+      r <- tt$r
+      # if (is.na(optimal_delta[r])) {
+      #   optimal_delta[r] <- compute_optimal_delta_given_r(r = r, gamma = Gama, kappa = Kappa, a = Alpha, n = n)
+      # }
+      idx <- tt$idx
+      Allocs[[max(1, j - 1)]] <- idx
+      if (Gama != 0) {
+        # u <- gs3(u, n = n, r = r, alpha = Alpha, beta = Kappa, gama = Gama, delta = optimal_delta[r])
+        # delta_U <- optimal_delta[r]
+        tmp = gs3_adaptive3(ut = u, n = n, r = r, alpha = Alpha, beta = Kappa, gama = Gama, delta = delta_U, U = U, iter = j, adapt = adaptive)
+        u = tmp$u_prime
+        delta_U = tmp$delta
+      }
+      JiC <- MvInv(
+        eps = Meps, u = u, alpha = Alpha, beta = Kappa,
+        gama = Gama, N = 50001
+      )
+      Nm <- length(JiC)
+      TauiC <- rk(Nm, distr = distr.p0, mu = mu.p0, sigma = sigma.p0)
+      ystar <- gs4(ystar, x, idx,
+                   distr.k = distr.k, sigma.k = sigma,
+                   distr.p0 = distr.p0, mu.p0 = mu.p0, sigma.p0 = sigma.p0
+      )
+      Jstar <- rgamma(r, nstar - Gama, Kappa + u)
+      Tau <- c(TauiC, ystar)
+      J <- c(JiC, Jstar)
+      tt <- gsHP(ystar, r, distr.p0)
+      mu.p0 <- tt$mu.py0
+      sigma.p0 <- tt$sigma.py0
+      y <- fcondYXA(x, distr = distr.k, Tau = Tau, J = J, sigma = sigma)
+      sigma <- gs5(sigma, x, y,
+                   distr = distr.k, asigma = asigma,
+                   bsigma = bsigma, delta = delta_S
+      )
+      Fxx[, j] <- fcondXA(xx,
+                          distr = distr.k, Tau = Tau, J = J,
+                          sigma = sigma
+      )
+      fx[, j] <- fcondXA(x,
+                         distr = distr.k, Tau = Tau, J = J,
+                         sigma = sigma
+      )
+      R[j] <- r
+      S[j] <- sigma
+      U[j] <- u
+      Nmt[j] <- Nm
+      if (extras) {
+        means[[j]] <- Tau
+        weights[[j]] <- J / sum(J)
+        Js[[j]] <- J
+        delta_Us[j] <- delta_U
+      }
+    }
+    tt <- comp1(y)
+    Allocs[[Nit]] <- tt$idx
+    biseq <- seq(floor(Pbi * Nit))
+    Fxx <- Fxx[, -biseq]
+    qx <- as.data.frame(t(apply(Fxx, 1, quantile, probs = probs)))
+    names(qx) <- paste("q", probs, sep = "")
+    qx <- cbind(mean = apply(Fxx, 1, mean), qx)
+    R <- R[-biseq]
+    S <- S[-biseq]
+    U <- U[-biseq]
+    Allocs <- Allocs[-biseq]
+    if (extras) {
+      means <- means[-biseq]
+      weights <- weights[-biseq]
+      Js <- Js[-biseq]
+      delta_Us <- delta_Us[-biseq]
+    }
+    cpo <- 1 / apply(1 / fx[, -biseq], 1, mean)
+    if (printtime) {
+      cat(" >>> Total processing time (sec.):\n")
+      print(procTime <- proc.time() - tInit)
+    }
+    res <- list(
+      xx = xx, qx = qx, cpo = cpo, R = R, S = S,
+      U = U, Allocs = Allocs, Nm = Nmt, Nx = Nx, Nit = Nit,
+      Pbi = Pbi, procTime = procTime, distr.k = distr.k, data = x,
+      NRMI_params = list("Alpha" = Alpha, "Kappa" = Kappa, "Gamma" = Gama)
+    )
+    if (extras) {
+      res$means <- means
+      res$weights <- weights
+      res$Js <- Js
+      res$delta_Us <- delta_Us
+    }
+    return(structure(res, class = "NRMI1"))
+  }
 
 
 
--- R/MixNRMI2.R
+++ R/MixNRMI2.R
@@ -319,7 +319,156 @@ MixNRMI2 <-
     return(structure(res, class = "NRMI2"))
   }
 
-
+MixNRMI2bis <-
+  function(x, probs = c(0.025, 0.5, 0.975), Alpha = 1, Kappa = 0,
+           Gama = 0.4, distr.k = "normal", distr.py0 = "normal", distr.pz0 = "gamma", mu.pz0 = 3,
+           sigma.pz0 = sqrt(10), delta_S = 4, kappa = 2, delta_U = 2, Meps = 0.01,
+           Nx = 150, Nit = 1500, Pbi = 0.1, epsilon = NULL, printtime = TRUE,
+           extras = TRUE, adaptive = FALSE) {
+    if (is.null(distr.k)) {
+      stop("Argument distr.k is NULL. Should be provided. See help for details.")
+    }
+    if (is.null(distr.py0)) {
+      stop("Argument distr.py0 is NULL. Should be provided. See help for details.")
+    }
+    distr.k <- process_dist_name(distr.k)
+    distr.py0 <- process_dist_name(distr.py0)
+    distr.pz0 <- process_dist_name(distr.pz0)
+    tInit <- proc.time()
+    n <- length(x)
+    y <- x
+    xsort <- sort(x)
+    y[seq(n / 2)] <- mean(xsort[seq(n / 2)])
+    y[-seq(n / 2)] <- mean(xsort[-seq(n / 2)])
+    z <- rep(1, n)
+    u <- 1
+    if (is.null(epsilon)) {
+      epsilon <- sd(x) / 4
+    }
+    xx <- seq(min(x) - epsilon, max(x) + epsilon, length = Nx)
+    Fxx <- matrix(NA, nrow = Nx, ncol = Nit)
+    fx <- matrix(NA, nrow = n, ncol = Nit)
+    R <- seq(Nit)
+    U <- seq(Nit)
+    Nmt <- seq(Nit)
+    Allocs <- vector(mode = "list", length = Nit)
+    if (!adaptive) {
+      optimal_delta <- rep(delta_U, n)
+    }
+    else {
+      optimal_delta <- rep(NA, n)
+    }
+    if (extras) {
+      means <- vector(mode = "list", length = Nit)
+      sigmas <- vector(mode = "list", length = Nit)
+      weights <- vector(mode = "list", length = Nit)
+      Js <- vector(mode = "list", length = Nit)
+      delta_Us <- seq(Nit)
+    }
+    mu.py0 <- mean(x)
+    sigma.py0 <- sd(x)
+    for (j in seq(Nit)) {
+      if (floor(j / 500) == ceiling(j / 500)) {
+        cat("MCMC iteration", j, "of", Nit, "\n")
+      }
+      tt <- comp2(y, z)
+      ystar <- tt$ystar
+      zstar <- tt$zstar
+      nstar <- tt$nstar
+      rstar <- tt$rstar
+      idx <- tt$idx
+      Allocs[[max(1, j - 1)]] <- idx
+      # if (is.na(optimal_delta[rstar])) {
+      #   optimal_delta[rstar] <- compute_optimal_delta_given_r(r = rstar, gamma = Gama, kappa = Kappa, a = Alpha, n = n)
+      # }
+      if (Gama != 0) {
+        # u_delta_U <- gs3_adaptive2(u, n = n, r = rstar, alpha = Alpha, beta = Kappa, gama = Gama, delta = delta_U, U = U, iter = j, adapt = adaptive)
+        # u <- u_delta_U$u_prime
+        # delta_U <- u_delta_U$delta
+        # u <- gs3(u, n = n, r = rstar, alpha = Alpha, beta = Kappa, gama = Gama, delta = optimal_delta[rstar])
+        # delta_U <- optimal_delta[rstar]
+        tmp = gs3_adaptive3(u, n = n, r = rstar, alpha = Alpha, beta = Kappa, gama = Gama, delta = delta_U, U = U, iter = j, adapt = adaptive)
+        u = tmp$u_prime
+        delta_U = tmp$delta
+      }
+      JiC <- MvInv(
+        eps = Meps, u = u, alpha = Alpha, beta = Kappa,
+        gama = Gama, N = 50001
+      )
+      Nm <- length(JiC)
+      TauyC <- rk(Nm, distr = distr.py0, mu = mu.py0, sigma = sigma.py0)
+      TauzC <- rk(Nm, distr = distr.pz0, mu = mu.pz0, sigma = sigma.pz0)
+      tt <- gsYZstar(ystar, zstar, nstar, rstar, idx, x, delta_S,
+                     kappa,
+                     distr.k = distr.k, distr.py0 = distr.py0,
+                     mu.py0 = mu.py0, sigma.py0 = sigma.py0, distr.pz0 = distr.pz0,
+                     mu.pz0 = mu.pz0, sigma.pz0 = sigma.pz0
+      )
+      ystar <- tt$ystar
+      zstar <- tt$zstar
+      tt <- gsHP(ystar, rstar, distr.py0)
+      mu.py0 <- tt$mu.py0
+      sigma.py0 <- tt$sigma.py0
+      Jstar <- rgamma(rstar, nstar - Gama, Kappa + u)
+      Tauy <- c(TauyC, ystar)
+      Tauz <- c(TauzC, zstar)
+      J <- c(JiC, Jstar)
+      tt <- fcondYZXA(x, distr = distr.k, Tauy, Tauz, J)
+      y <- tt[, 1]
+      z <- tt[, 2]
+      Fxx[, j] <- fcondXA2(xx,
+                           distr = distr.k, Tauy, Tauz,
+                           J
+      )
+      fx[, j] <- fcondXA2(x, distr = distr.k, Tauy, Tauz, J)
+      R[j] <- rstar
+      U[j] <- u
+      Nmt[j] <- Nm
+      if (extras) {
+        means[[j]] <- Tauy
+        sigmas[[j]] <- Tauz
+        weights[[j]] <- J / sum(J)
+        Js[[j]] <- J
+        delta_Us[j] <- delta_U
+      }
+    }
+    tt <- comp2(y, z)
+    Allocs[[Nit]] <- tt$idx
+    biseq <- seq(floor(Pbi * Nit))
+    Fxx <- Fxx[, -biseq]
+    qx <- as.data.frame(t(apply(Fxx, 1, quantile, probs = probs)))
+    names(qx) <- paste("q", probs, sep = "")
+    qx <- cbind(mean = apply(Fxx, 1, mean), qx)
+    R <- R[-biseq]
+    U <- U[-biseq]
+    Allocs <- Allocs[-biseq]
+    if (extras) {
+      means <- means[-biseq]
+      sigmas <- sigmas[-biseq]
+      weights <- weights[-biseq]
+      Js <- Js[-biseq]
+      delta_Us <- delta_Us[-biseq]
+    }
+    cpo <- 1 / apply(1 / fx[, -biseq], 1, mean)
+    if (printtime) {
+      cat(" >>> Total processing time (sec.):\n")
+      print(procTime <- proc.time() - tInit)
+    }
+    res <- list(
+      xx = xx, qx = qx, cpo = cpo, R = R, U = U,
+      Allocs = Allocs, Nm = Nmt, Nx = Nx, Nit = Nit, Pbi = Pbi,
+      procTime = procTime, distr.k = distr.k, data = x,
+      NRMI_params = list("Alpha" = Alpha, "Kappa" = Kappa, "Gamma" = Gama)
+    )
+    if (extras) {
+      res$means <- means
+      res$sigmas <- sigmas
+      res$weights <- weights
+      res$Js <- Js
+      res$delta_Us <- delta_Us
+    }
+    return(structure(res, class = "NRMI2"))
+  }
 
 #' Plot the density estimate and the 95\% credible interval
 #'
